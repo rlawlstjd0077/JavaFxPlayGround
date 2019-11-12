9c179863953a83a98dcd967e005263581c3aa819
JSS <rlawlstjd077@gmail.com>
2019-11-12 23:34:17 +0900

    add tray menu

---
+++ src/main/java/test/TrayMenuLauncher.java
@@ -0,0 +1,27 @@
+package test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.InputStream;
+import java.util.Locale;
+
+public class TrayMenuLauncher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final InputStream resource = this.getClass().getResourceAsStream("/icons/tray_icon.png");
+        TrayIcon trayIcon = new TrayIcon(ImageIO.read(resource));
+
+        TrayMenu trayMenu = new TrayMenu(primaryStage, trayIcon);
+        trayMenu.addAppToTray();
+        while(true) {
+
+        }
+    }
+}
---
+++ src/test/java/test/TrayMenuTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TrayMenuTest extends Application {
+    @Test
+    @Ignore
+    public void test() {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        final InputStream resource = this.getClass().getResourceAsStream("/test/icons/tray_icon.png");
+        TrayIcon trayIcon = new TrayIcon(ImageIO.read(resource));
+
+        TrayMenu trayMenu = new TrayMenu(primaryStage, trayIcon);
+        trayMenu.addAppToTray();
+        while(true) {
+
+        }
+    }
+}
+
